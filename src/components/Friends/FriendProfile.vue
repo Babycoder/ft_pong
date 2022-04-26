f3863286a47ea8653194de1282e4eddb52a8efce
Aymen Ghazali <ayghazal@e2r9p9.1337.ma>
2022-04-26 16:39:11 +0000

    potonat

--- src/components/Friends/FriendProfile.vue
+++ src/components/Friends/FriendProfile.vue
@@ -23,6 +23,10 @@
         >
            Play
         </button>
+        <div  class="w-28  bg-blue-500 rounded-lg font-bold text-white text-center px-4 py-3 transition duration-300 ease-in-out hover:bg-blue-600 mr-6"
+        >
+            Add
+        </div>
         </div>
         <p> Wins : {{user.wins}}</p>
         <p> Loses : {{user.loses}}</p>
